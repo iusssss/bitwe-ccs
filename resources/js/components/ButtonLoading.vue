b97049fd1d6d6d165eda047ad36dc8b52bb51320
Julius <decena.juliusm@gmail.com>
2019-11-23 14:21:34 +0800

    Add button loading component

---
+++ resources/js/components/ButtonLoading.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<span v-if="loading">
+	        <i class="fas fa-spinner fa-pulse"></i>
+	        {{ loadingText }}
+	    </span>
+		<span v-else>
+			{{ defaultText }}
+		</span>
+	</div>
+</template>
+<script>
+	export default {
+		props: ['loading', 'loadingText', 'defaultText']
+	}
+</script>
